ed49c6f96fee279cf8e5bda06b7f23a8a25d6f1a
hongjian <useremail@qq.com>
2022-09-01 20:18:57 +0800

    创建主窗口

---
+++ PyQt5基本窗口控件/qt402_Center.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5.QtWidgets import QDesktopWidget, QApplication, QMainWindow
+
+
+class WinForm(QMainWindow):
+    def __init__(self):
+        super(WinForm, self).__init__()
+        self.setWindowTitle("主窗口放在屏幕中间例子")
+        self.resize(370, 250)
+        self.center()
+
+    def center(self):
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = WinForm()
+    form.show()
+    sys.exit(app.exec_())
