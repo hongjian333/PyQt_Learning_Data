c2e5a3ac6c17cf6d679f7954cbe59c1cb99971a6
hongjian <useremail@qq.com>
2022-09-03 12:42:02 +0800

    QCheckBox的使用

---
+++ PyQt5基本窗口控件/qt416_QCheckBox.py
@@ -0,0 +1,54 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QCheckBox
+
+
+class CheckBoxDemo(QWidget):
+    def __init__(self):
+        super(CheckBoxDemo, self).__init__()
+
+        groupBox = QGroupBox("checkBoxes")
+        groupBox.setFlat(True)
+
+        formLayout = QHBoxLayout()
+
+        self.checkBox1 = QCheckBox("&CheckBox1")
+        self.checkBox1.setChecked(True)
+        self.checkBox1.stateChanged.connect(lambda: self.btnState(self.checkBox1))
+        formLayout.addWidget(self.checkBox1)
+
+        self.checkBox2 = QCheckBox("CheckBox2")
+        self.checkBox2.toggled.connect(lambda: self.btnState(self.checkBox2))
+        formLayout.addWidget(self.checkBox2)
+
+        self.checkBox3 = QCheckBox("CheckBox3")
+        self.checkBox3.setTristate(True)
+        self.checkBox3.setCheckState(Qt.PartiallyChecked)
+        self.checkBox3.stateChanged.connect(lambda: self.btnState(self.checkBox3))
+        formLayout.addWidget(self.checkBox3)
+
+        groupBox.setLayout(formLayout)
+        miaiLayout = QVBoxLayout()
+        miaiLayout.addWidget(groupBox)
+
+        self.setLayout(miaiLayout)
+        self.setWindowTitle("CheckBox Demo")
+
+    def btnState(self, btn: QCheckBox):
+        chk1Status = self.checkBox1.text() + ", isChecked=" + str(self.checkBox1.isChecked()) + ", checkState=" + str(
+            self.checkBox1.checkState()) + "\n"
+        chk2Status = self.checkBox2.text() + ", isChecked=" + str(self.checkBox2.isChecked()) + ", checkState=" + str(
+            self.checkBox2.checkState()) + "\n"
+        chk3Status = self.checkBox3.text() + ", isChecked=" + str(self.checkBox3.isChecked()) + ", checkState=" + str(
+            self.checkBox3.checkState()) + "\n"
+        print(chk1Status, chk2Status, chk3Status)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = CheckBoxDemo()
+    form.show()
+    sys.exit(app.exec_())
